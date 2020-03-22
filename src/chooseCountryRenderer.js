53b6f1e7b3abd14ac6b1091f433daec9f93111b6
michael <michale.ns32@gmail.com>
2020-03-22 12:51:50 +0200

    add search to country selector

--- src/chooseCountryRenderer.js
+++ src/chooseCountryRenderer.js
@@ -3,7 +3,25 @@ const { ipcRenderer } = require('electron')
 
 const form = document.getElementById("countrySelector")
 
+const search = document.getElementById("search")
+search.oninput = function(e) {
+    const filter = e.target.value.toLowerCase()
+    const radios = form.getElementsByClassName("radio-container")
+
+    for (let i=0; i < radios.length; i++) {
+        if (radios[i].getElementsByTagName("input")[0].value.toLowerCase().indexOf(filter) === -1) {
+            radios[i].style.display = "none"
+        } else {
+            radios[i].style.display = "block"
+        }
+    }
+}
+
 function createRadio(name) {
+    const container = document.createElement("div")
+    container.classList.add("radio-container")
+    container.setAttribute("id", name + "-container")
+
     const input = document.createElement("input")
     input.setAttribute("type", "radio")
     input.setAttribute("id", name)
@@ -14,14 +32,14 @@ function createRadio(name) {
         ipcRenderer.send('country-updated', e.target.value)
     })
 
-    form.appendChild(input)
+    container.appendChild(input)
 
     const label = document.createElement("label")
     label.setAttribute("for", name)
     label.textContent = name
-    form.appendChild(label)
+    container.appendChild(label)
 
-    form.appendChild(document.createElement("br"))
+    form.appendChild(container)
 }
 
 async function fetchData() {
